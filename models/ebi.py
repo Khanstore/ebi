5813f67c9fd097c2cb86505aec3f35e1c1dcff7d
Khanstore <dmashraf@gmail.com>
2025-01-10 15:26:06 +0600

    import jsonfields perfect

--- models/ebi.py
+++ models/ebi.py
@@ -1,33 +1,48 @@
 # -*- coding: utf-8 -*-
+from pathlib import Path
 import csv,psycopg2
 import io
 import base64
 import xmlrpc.client
 from odoo import models, fields
 import os,pandas as pd
+import json
 
 
 
-class ebiFields(models.TransientModel):
+class ebiFields(models.Model):
     _name = 'ebi.fields'
     _description = 'fields to import'
 
     name = fields.Char("Name")
-    model_id = fields.Many2one('ir.model', string='Model', required=True)
+    sequence = fields.Integer("Sequence" ,default=10)
+    model_id = fields.Many2one('ebi.model', string='Model', required=True)
     source = fields.Char("Source")
     target = fields.Char("target")
+    default_value=fields.Char('DefaultValue')
+
+class ebiModel(models.Model):
+    _name = 'ebi.model'
+    _description = 'Models to import'
+    sequence=fields.Integer("Sequence" ,default=10)
+    name = fields.Char("Name")
+    database_id = fields.Many2one('ebi.database', string='database', required=True)
+    source = fields.Char("Source")
+    target = fields.Char("target")
+    data_type = fields.Char("Data Type")
+    # fixme apply this domain, domain="[('model_id', '=', model_id)]")
+    field_ids = fields.Many2many('ebi.fields', string='Fields')
 
 
 
 
-class ExportDataWizard(models.TransientModel):
-    _name = 'export.data.wizard'
+class ExportDataWizard(models.Model):
+    _name = 'ebi.database'
     _description = 'Export Data with External IDs (XML-RPC)'
 
     source_address = fields.Char(string="Source Address", required=True, default="http://localhost:8069")
     source_database = fields.Char(string="Source Database", required=True)
     source_email = fields.Char(string="User Eamil", required=True)
-    server_path = fields.Char(string="server_path", required=True)
     source_uid = fields.Integer(string="Source UID")
     source_pass = fields.Char(string="Source Password", required=True)
 
@@ -37,17 +52,40 @@ class ExportDataWizard(models.TransientModel):
     target_uid = fields.Integer(string="Target UID")
     target_pass = fields.Char(string="Target Password", required=True)
 
-    postgre_uid = fields.Char(string="postgree User")
-    postgre_pass = fields.Char(string="Postgree Password", required=True)
+    source_pg_user = fields.Char(string="postgree User")
+    source_pg_port = fields.Char(string="postgree port")
+    source_pg_pass = fields.Char(string="Postgree Password", required=True)
+    source_pg_host=fields.Char("Host")
 
+    target_pg_user = fields.Char(string="postgree User")
+    target_pg_port = fields.Char(string="postgree port")
+    target_pg_pass = fields.Char(string="Postgree Password", required=True)
+    target_pg_host=fields.Char("Host")
+
+
+    def get_default_downloads_path(self):
+        """
+        Gets the default downloads folder path in Windows.
+
+        Returns:
+          str: The path to the downloads folder.
+        """
+        try:
+            return str(Path.home() / "Downloads")
+        except:
+            return None  # Handle potential errors
+
+    download_path = fields.Char(string="download_path", required=True, default=get_default_downloads_path)
     export_type=fields.Selection([('download', 'Download Only'),
                                     ('direct', 'Direct DB to DB'),
                                     ('server', 'Export')],
                                    string='Backup Type')
 
-    model_id = fields.Many2one('ir.model', string='Model', required=True)
+    model_ids = fields.Many2many('ebi.model','ebi_database_model_rel','database_id','model_ids', string='Model')
     # field_ids = fields.Many2many('ir.model.fields', string='Fields', domain="[('model_id', '=', model_id)]")
-    field_ids = fields.Many2many('ebi.fields', string='Fields', domain="[('model_id', '=', model_id)]")
+    # field_ids = fields.Many2many('ebi.fields', string='Fields', domain="[('model_id', '=', model_id)]")
+
+
 
     def direct_db_db_export(self):
         pass
@@ -70,6 +108,81 @@ class ExportDataWizard(models.TransientModel):
 
     def export_csv(self):
         model_name = self.model_id.model
+
+    import xmlrpc.client
+    import psycopg2
+    import pandas as pd
+    def create_connection(self,db_type):
+        if db_type=="source":
+            database=self.source_database
+            password = self.source_pg_pass
+            host=self.source_pg_host
+            user=self.source_pg_user
+            port = self.source_pg_port
+        else:
+            database=self.target_database
+            password = self.target_pg_pass
+            host=self.target_pg_host
+            user=self.target_pg_user
+            port = self.target_pg_port
+        conn=psycopg2.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password,
+            port=port)
+        return conn
+
+    def export_jsondata(self):
+        source_conn = self.create_connection("source")
+        target_conn = self.create_connection("Target")
+        source_cur = source_conn.cursor()
+        target_cur = target_conn.cursor()
+
+        for model in self.model_ids:
+            # fixme create logic for different source and target table name
+            source_table=model.source.replace(".",'_')
+            target_table=model.target.replace(".",'_')
+            # fixme get list of source fields here
+            source_fields = [field.source for field in model.field_ids if field.source]
+            target_fields = [field.target for field in model.field_ids if field.source]
+
+            source_cur.execute(f"SELECT {','.join(source_fields)} FROM {source_table}")
+            source_res=source_cur.fetchall()
+
+            df = pd.DataFrame.from_dict(source_res)
+            #since sourece res doesnt contain column name
+            # following line adds collumn names
+            df = df.rename(columns={index: value for index, value in enumerate(target_fields)})
+
+            #format json type data
+            target_cur.execute(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{target_table}';")
+            data_type=target_cur.fetchall()
+            for line in data_type:
+                if line[1]=="jsonb": # line(1) is data_type
+                    if line[0] in target_fields:
+                        df[line[0]] = df[line[0]].apply(json.dumps)
+            # fixme here to insert the required fields default values to dataframe and frmat json fields
+            fields_to_add=[field.target for field in model.field_ids if not field.source]
+            value_to_add=[field.default_value for field in model.field_ids if not field.source]
+            number_of_fields=len(target_fields)+len(fields_to_add)
+            target_fields=target_fields+fields_to_add
+            index=0
+            for line in fields_to_add:
+                df[line]=value_to_add[index]
+                index=index+1
+
+            data = [tuple(row) for row in df.itertuples(index=False)]
+
+            for rec in data:
+                target_cur.execute(
+                f"INSERT INTO {target_table} ({','.join(target_fields)}) VALUES ({', '.join(['%s'] * number_of_fields)})",  rec)
+        target_conn.commit()
+
+
+
+
+
     def export_data(self):
         # IRMD for ir.model.data
         IRMD = {'source': {}, 'destination': {}}
@@ -89,12 +202,9 @@ class ExportDataWizard(models.TransientModel):
             target_db = self.target_database
 
             model_name = self.model_id.model
-            fields_to_export = []
-            fields_to_import = []
-            fields_to_export.extend([field.source for field in self.field_ids])
-            fields_to_import.extend([field.target for field in self.field_ids])
+            fields_to_export = [field.source for field in self.field_ids]
+            fields_to_import = [field.target for field in self.field_ids]
 
-            # Fetch data using XML-RPC and pagination
             limit = 1000
             offset = 0
             all_source_records = []
@@ -102,45 +212,54 @@ class ExportDataWizard(models.TransientModel):
             target_connection = psycopg2.connect(
                 host="localhost",
                 database=self.target_database,
-                user=self.postgre_uid,
+                user=self.source_pg_user,
                 password=self.postgre_pass,
                 port='5432'
             )
             target_connection.autocommit = True
             target_cr = target_connection.cursor()
 
-
-
-
-
             while True:
-                res = source_object.execute_kw(db, uid, password, model_name, 'search_read', [[('id', '>', 0)]],
-                                                {'fields': fields_to_export, 'order': 'id asc', 'limit': limit, 'offset': offset})
+                res = source_object.execute_kw(db, uid, password, model_name, 'search_read',
+                                               [[('id', '>', 0)]],
+                                               {'fields': fields_to_export, 'order': 'id asc', 'limit': limit,
+                                                'offset': offset})
                 if not res:
                     break
                 all_source_records.extend(res)
                 offset += limit
 
-            # fixme here to correct the field names which is different from the source
-            df=pd.DataFrame.from_dict(all_source_records)
-            df=df.rename(columns={key: value for key, value in zip(fields_to_export, fields_to_import)})
+            df = pd.DataFrame.from_dict(all_source_records)
+            df = df.rename(columns={key: value for key, value in zip(fields_to_export, fields_to_import)})
             all_source_records = df.to_dict('records')
+
             if self.export_type == 'direct':
+                # alter id sequence of the table
+                target_cr.execute(f"ALTER SEQUENCE {self.model_id.model.replace('.', '_')}_id_seq OWNED BY NONE")
+                self.insert_pandas_to_postgres(df, model_name, "localhost", self.target_database, self.source_pg_user, self.postgre_pass)
+
+
                 for record in all_source_records:
-                    targ = target_object.execute_kw(target_db, target_uid, target_password, model_name, 'search_read',
-                                                    [[('id', '=', record['id'])]])
+                    targ = target_object.execute_kw(target_db, target_uid, target_password, model_name,
+                                                    'search_read', [[('id', '=', record['id'])]])
                     if not targ:
-
-
-                        targ=target_object.execute_kw(target_db, target_uid, target_password, model_name,
-                                                        'create',[record])
-                        # update ID
-                        target_cr.execute("update " + self.model_id.model.replace('.','_')  +" set id="+str(record['id']) +" where id ="+str(targ))
-
+                        targ_id = target_object.execute_kw(target_db, target_uid, target_password, model_name,
+                                                           'create', [record])
+                        # target_cr.execute(
+                        #     f"UPDATE {self.model_id.model.replace('.', '_')} SET id = {record['id']} WHERE id = {targ_id}")
                     else:
-                        # update record
-                        targ=target_object.execute_kw(target_db, target_uid, target_password, model_name,
-                                                 'write',[[targ[0]['id']], record] )
+                        target_object.execute_kw(target_db, target_uid, target_password, model_name,
+                                                 'write', [[targ[0]['id']], record])
+                # alter id sequence of the table
+                target_cr.execute(
+                    f"ALTER SEQUENCE {self.model_id.model.replace('.', '_')}_id_seq OWNED BY {self.model_id.model.replace('.', '_')}.id;")
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            if target_connection:
+                target_cr.close()
+                target_connection.close()
+                print("Target PostgreSQL connection is closed")
 
 
             elif self.export_type == 'download':
@@ -195,7 +314,7 @@ class ExportDataWizard(models.TransientModel):
                 }
             elif self.export_type == 'server':
                 filename = f'{model_name}.csv'
-                save_path = self.server_path
+                save_path = self.download_path
                 if not os.path.exists(save_path):
                     os.makedirs(save_path)
                 full_path = os.path.join(save_path, filename)
@@ -257,8 +376,8 @@ class ExportDataWizard(models.TransientModel):
                         'sticky': True
                     }
 
-        except Exception as e:
-            print(f"Error during XML-RPC export: {e}")
+        # except Exception as e:
+        #     print(f"Error during XML-RPC export: {e}")
             # return {
             #     'type': 'ir.ui.message',
             #     'title': 'Error',
@@ -317,3 +436,58 @@ class ExportDataWizard(models.TransientModel):
             offset += limit
 
         return all_source_records
+
+    def insert_pandas_to_postgres(self,df, table_name, host, database, user, password):
+        """
+        Inserts all records from a pandas DataFrame into a PostgreSQL table.
+
+        Args:
+          df: pandas DataFrame containing the data to be inserted.
+          table_name: Name of the target table in the PostgreSQL database.
+          host: Hostname or IP address of the PostgreSQL server.
+          database: Name of the PostgreSQL database.
+          user: Username for the PostgreSQL database.
+          password: Password for the PostgreSQL database.
+
+        Returns:
+          None
+        """
+
+        try:
+            conn = psycopg2.connect(
+                host="localhost",
+                database=database,
+                user=self.source_pg_user,
+                password=self.postgre_pass
+            )
+            cur = conn.cursor()
+
+            # Construct the SQL INSERT statement dynamically
+            columns = ', '.join(df.columns)
+            placeholders = ', '.join(['%s'] * len(df.columns))  # Correctly create placeholders
+            sql = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+
+            # Use psycopg2.extras.execute_values for efficient bulk insertion
+            psycopg2.extras.execute_values(cur, sql, list(df.itertuples(index=False, name=None)))
+            conn.commit()
+            print("Data inserted successfully!")
+
+        except (Exception, psycopg2.Error) as error:
+            print(f"Error while inserting data to PostgreSQL: {error}")
+
+        except (Exception, psycopg2.Error) as error:
+            print(f"Error while inserting data to PostgreSQL: {error}")
+
+        finally:
+            # Close the communication with the PostgreSQL
+            if conn:
+                cur.close()
+                conn.close()
+                print("PostgreSQL connection is closed")
+# cur.execute(sql)
+# # Fetch all rows from the executed query
+# rows = cursor.fetchall()
+# for row in rows: # Assuming 'name' is a JSON field, parse it using json.loads
+# id = row[0]
+# name_json = json.loads(row[1])
+# print(f"ID: {id}, Name: {name_json}"
